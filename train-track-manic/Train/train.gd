52a59e787d6303dfeae6814c63d6b3f9d2a0628e
Loczy <deadlocz26@gmail.com>
2026-04-25 14:51:51 +0200

    le trains with le pathfinding

---
+++ train-track-manic/Train/train.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+var speed = 500.0
+
+@onready var nav: NavigationAgent2D = $NavigationAgent2D
+@onready var station: CharacterBody2D = $"../Station"
+
+# set le movement target
+func set_movement_target(movement_target: Vector2):
+	nav.target_position = movement_target
+
+func _physics_process(delta: float) -> void:
+	_move_towards_station() # move towards le station
+
+# move towards la station
+func _move_towards_station() -> void:
+	# update the station position
+	set_movement_target(station.position)
+	
+	# if we're at the target, stop
+	if nav.is_navigation_finished():
+		return 
+	
+	# get pathfinding information
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = nav.get_next_path_position()
+	var new_velocity = current_agent_position.direction_to(next_path_position) * speed
+
+	# calculate the new velocity if avoidance is enabled
+	if nav.avoidance_enabled:
+		nav.set_velocity(new_velocity)
+	else:
+		_velocity_computed(new_velocity)
+	
+	# do la movement
+	move_and_slide()
+
+func _velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
