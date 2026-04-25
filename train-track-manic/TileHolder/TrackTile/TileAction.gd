381028a77161ed0973e26c2a8f5745634687e843
DW215 <alexweiss999@gmail.com>
2026-04-25 23:02:49 +0200

    mark tile

--- train-track-manic/TileHolder/TrackTile/TileAction.gd
+++ train-track-manic/TileHolder/TrackTile/TileAction.gd
@@ -1,36 +1,41 @@
-class_name TrackTile extends Sprite2D
+class_name TrackTile
+extends Sprite2D
 
-var tile_type:int
+var tile_type: int
 @export var rect: Rect2
 @export var level: Node2D
+@export var is_placed2: bool = false
 
+const SOURCE_ID := 0
 
 func set_tile(type: int) -> void:
 	tile_type = type
-	var y = type / 4
-	var x = type % 4
-	%TileMapLayer.set_cell(Vector2i(0, 0), -1, Vector2i(x, y), 0)
-	set_logic(type)
+	var y := type / 4
+	var x := type % 4
+	%TileMapLayer.set_cell(Vector2i(0, 0), SOURCE_ID, Vector2i(x, y), 0)
 
-func set_logic(type: int):
-	#TODO add logic depending on what is needed
-	match(type):
-		0:
-			pass
-		1:
-			pass
-
-func get_global_rect():
+func get_global_rect() -> Rect2:
 	return Rect2(
 		global_position - rect.size / 2,
 		rect.size
 	)
-	
-func set_on_place():
-	modulate.a = 1
 
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-			Event.move_tile.emit(self)
-		
+func set_on_place():
+	is_placed2 = true
+	modulate.a = 1
+	set_as_top_level(false)
+
+	if has_node("Area2D"):
+		$Area2D.input_pickable = false
+		$Area2D.monitoring = false
+		$Area2D.monitorable = false
+
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if is_placed2:
+		return
+
+	if event is InputEventMouseButton \
+	and event.button_index == MOUSE_BUTTON_LEFT \
+	and event.is_pressed():
+		is_placed2 = true
+		Event.create_new_tile.emit(self)
